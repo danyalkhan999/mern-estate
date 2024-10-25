f2ce19dbaa0d97a4d364be89dd25d690e3ab9d58
Danyal Khan <danyal.khan@ongraph.com>
2024-10-25 22:41:38 +0530

    connect to databse

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { connectDB } from "../config/mongo.js";
+import { configDotenv } from "dotenv";
 
+configDotenv();
 
 const app = express();
 
-
 app.listen(3000, () => {
-    console.log("server is running on port 3000!!!!");
-})
+  connectDB();
+  console.log("server is running on port 3000!!!!");
+});
