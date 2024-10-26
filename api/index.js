10d9c687022dc518d8b9415af4b5375a61cf1175
Danyal Khan <danyal.khan@ongraph.com>
2024-10-26 16:18:44 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { connectDB } from "../config/mongo.js";
+import { connectDB } from "./config/mongo.js";
 import { configDotenv } from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 configDotenv();
 
 const app = express();
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   connectDB();
