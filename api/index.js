831a6efac063923f4f242cb0c4e6a9a754a36d22
Danyal Khan <danyal.khan@ongraph.com>
2024-10-30 01:30:22 +0530

    create sign-up api route

--- api/index.js
+++ api/index.js
@@ -2,14 +2,19 @@ import express from "express";
 import { connectDB } from "./config/mongo.js";
 import { configDotenv } from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 configDotenv();
 
 const app = express();
 
-app.use("/api/user", userRouter);
+app.use(express.json());
 
 app.listen(3000, () => {
   connectDB();
   console.log("server is running on port 3000!!!!");
 });
+
+// routes
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
