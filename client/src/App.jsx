a5dc25495afeb3713556a7e86d6524d1b8add622
Danyal Khan <danyal.khan@ongraph.com>
2024-10-30 11:10:57 +0530

    complete signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
 import Profile from "./pages/Profile";
 import About from "./pages/about";
 import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
       </Routes>
